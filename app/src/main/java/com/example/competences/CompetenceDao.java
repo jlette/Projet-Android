077be889180650fc8dc769f6be180c92bc610830
jlettevoueto <lettevoueto.jimmy@gmail.com>
2021-03-18 17:18:08 +0100

    Projet android 18/03/20201

---
+++ app/src/main/java/com/example/competences/CompetenceDao.java
@@ -0,0 +1,22 @@
+package com.example.competences;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+
+public interface CompetenceDao {
+
+    @Insert
+    void insert(Competence competence);
+
+    @Query("DELETE FROM competence_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM competence_table ORDER BY nomCompetence ASC")
+    LiveData<List<Competence>> getToutesCompetences();
+}
